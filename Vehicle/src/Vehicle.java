84134084c842fd6cec65ab7f7d98039f799dcc82
Nikitostalkerby <hax2033@gmail.com>
2015-05-14 17:07:50 +0300

    Marka avto.
    Использование ключевого слова super() как вызов конструктора суперкласса.

---
+++ Vehicle/src/Vehicle.java
@@ -0,0 +1,27 @@
+class Vehicle {
+	private int passengers;
+	private int fuelcap;
+	private int mpg;
+
+
+	Vehicle(int passengers, int fuelcap, int mpg) {
+		this.passengers = passengers;
+		this.fuelcap = fuelcap;
+		this.mpg = mpg;
+	}
+	
+	int range() {
+		return mpg * fuelcap;
+	}
+	
+	double fuelneeded(int miles) {
+		return (double) miles / mpg;
+	}
+	
+	int getPassengers() { return this.passengers; }
+	void setPassengers(int passengers) { this.passengers = passengers; }
+	int getFuelcap() { return this.fuelcap; }
+	void setFuelcap(int fuelcap) { this.fuelcap = fuelcap; }
+	int getMpg() { return this.mpg; }
+	void setMpg(int mpg) { this.mpg = mpg; }
+}
