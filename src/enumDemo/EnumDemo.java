d300563f1963d9f2d1521a50ea3f5e96767d6b23
Nikitos Krivitski <hax2033@gmail.com>
2015-10-13 12:22:55 +0300

    Using values() and valueOf methods.

---
+++ src/enumDemo/EnumDemo.java
@@ -0,0 +1,19 @@
+package enumDemo;
+
+public class EnumDemo {
+
+	public static void main(String[] args) {
+		Apple ap;
+
+		System.out.println("Here are all Apple constants: ");
+
+		Apple allApples[] = Apple.values();
+
+		for (Apple a : allApples) {
+			System.out.println(a);
+		}
+
+		ap = Apple.valueOf("Winesap");
+		System.out.println("ap contains " + ap);
+	}
+}
