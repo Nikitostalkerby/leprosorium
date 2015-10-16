268e3f021df691fabf72238df6c2dac5fa5f21d1
Nikitos Krivitski <hax2033@gmail.com>
2015-10-16 18:25:14 +0800

    Extended version of copyFile.

---
+++ src/file/copyFileExt/Lepro.java
@@ -0,0 +1,30 @@
+package file.copyFileExt;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Lepro {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		int i;
+
+		try (FileInputStream fin = new FileInputStream(".//src/file/copyFileExt/input.txt");
+			 FileOutputStream fout = new FileOutputStream(".//src/file/copyFileExt/output.txt")) {
+			do {
+				i = fin.read();
+
+				if (i != -1) {
+					fout.write(i);
+				}
+			} while (i != -1);
+		} catch (IOException e) {
+
+			PrintWriter pw = new PrintWriter(System.out);
+			pw.println("I/O Error: " + e);
+		}
+	}
+}
