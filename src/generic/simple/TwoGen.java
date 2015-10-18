ffb1d4e85e10952ce2bfd15b56d3a1bfd690ff5d
Nikitos Krivitski <hax2033@gmail.com>
2015-10-18 15:50:03 +0300

    A Generic class with two type parameters.

---
+++ src/generic/simple/TwoGen.java
@@ -0,0 +1,26 @@
+package generic.simple;
+
+public class TwoGen<T, V> {
+
+	T ob1;
+	V ob2;
+
+	TwoGen(T o1, V o2) {
+		ob1 = o1;
+		ob2 = o2;
+	}
+
+	void showTypes() {
+		System.out.println("Type of T is " + ob1.getClass().getName());
+
+		System.out.println("Type of V is " + ob2.getClass().getName());
+	}
+
+	T getob1() {
+		return this.ob1;
+	}
+
+	V getob2() {
+		return this.ob2;
+	}
+}
