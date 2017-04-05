b0445cf38e015f9ed43d8b0e9303fd34b8e75a62
solairerove <hax2033@gmail.com>
2017-04-05 11:56:25 +0300

    Add example with using `flatMap`

--- src/main/java/com/github/solairerove/leprosorium/reactive/ReactiveExample.java
+++ src/main/java/com/github/solairerove/leprosorium/reactive/ReactiveExample.java
@@ -16,5 +16,10 @@ public class ReactiveExample {
                 e -> System.out.printf("Failed to process: %s\n", e),
                 () -> System.out.println("Done")
         );
+
+        Flowable.just("1").map(Integer::parseInt).subscribe(System.out::print);
+
+        Flowable.fromArray(1, 2, 3, 4).flatMap(
+                t -> Flowable.just(t + 50)).subscribe(System.out::print);
     }
 }
